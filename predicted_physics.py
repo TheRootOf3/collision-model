9b067a15780eee2efff5e1bc13ff87154c978375
TheRootOf3 <andrzej.szablewski.20@ucl.ac.uk>
2021-02-22 16:28:02 +0100

    changes ball position to vector

--- predicted_physics.py
+++ predicted_physics.py
@@ -28,12 +28,13 @@ class Table():
         self.friction = 1
         self.t_time = 0
 
-    def add_ball(self, ballID, x, y, r, vel_vector = np.array([1., 1.]), color = (255, 0, 0)):
-        self.balls.append(Ball(ballID, x, y, r, vel_vector, color))
+    def add_ball(self, ballID, pos, r, vel_vector = np.array([1., 1.]), color = (255, 0, 0)):
+        self.balls.append(Ball(ballID, pos, r, vel_vector, color))
     
     def update_table(self):
-        self.predict_collisions_wall(10)
         self.predict_collisions_ball(10)
+        self.predict_collisions_wall(10)
+
 
         self.check_ball_collisions()
         self.apply_friction()
@@ -48,20 +49,21 @@ class Table():
     def predict_collisions_wall(self, depth_time):
         for ball in self.balls:
             col_time, wall = self.calculate_wall_col_time(ball)
-            # self.set_ball_color(ball, wall)
+            self.set_ball_color(ball, wall)
             # print(self.return_new_wall_vector(ball, vector))
         # time.sleep(0.1)
         # time.sleep(1)
     
     def set_ball_color(self, ball, wall):
-        if wall == 0:
-            ball.color = (0,255,0)
-        elif wall == 1:
-            ball.color = (255,255,0)
-        elif wall == 2:
-            ball.color = (255,0,255)
-        elif wall == 3:
-            ball.color = (0,255,255)
+        if ball.color is not (255,0,0):
+            if wall == 0:
+                ball.color = (0,255,0)
+            elif wall == 1:
+                ball.color = (255,255,0)
+            elif wall == 2:
+                ball.color = (255,0,255)
+            elif wall == 3:
+                ball.color = (0,255,255)
 
     def return_new_wall_vector(self, ball, wall):
         if wall < 2:
@@ -85,12 +87,10 @@ class Table():
         # time.sleep(0.01)
 
     def calculate_ball_col_time(self, ball1, ball2):
-        c1 = np.array([ball1.x, ball1.y])
-        c2 = np.array([ball2.x, ball2.y])
-        
+       
         p = [np.dot((ball1.vel_vector - ball2.vel_vector), (ball1.vel_vector - ball2.vel_vector)),
-            2*np.dot(ball1.vel_vector - ball2.vel_vector, c1 - c2),
-            np.dot(c1 - c2, c1 - c2) - (ball1.r + ball2.r)**2
+            2*np.dot(ball1.vel_vector - ball2.vel_vector, ball1.pos - ball2.pos),
+            np.linalg.norm(ball1.pos - ball2.pos)**2 - (ball1.r + ball2.r)**2
         ]
 
         roots = np.roots(p)
@@ -109,8 +109,8 @@ class Table():
     def check_calculated_time(self, time, ball):
         if time < 0 or isinstance(time, complex):
             return False
-        if ball.x + time*ball.vel_vector[0] >= 0 + ball.r and ball.x + time*ball.vel_vector[0] <= DIM_BOARD_X - ball.r and\
-            ball.y + time*ball.vel_vector[1] >= 0 + ball.r and ball.y + time*ball.vel_vector[1] <= DIM_BOARD_Y - ball.r:
+        if ball.pos[0] + time*ball.vel_vector[0] >= 0 + ball.r and ball.pos[0] + time*ball.vel_vector[0] <= DIM_BOARD_X - ball.r and\
+            ball.pos[1] + time*ball.vel_vector[1] >= 0 + ball.r and ball.pos[1] + time*ball.vel_vector[1] <= DIM_BOARD_Y - ball.r:
             return True
         return False
 
@@ -118,12 +118,12 @@ class Table():
     def calculate_wall_col_time(self, ball):
         times = [1000000, 1000000, 1000000, 1000000]
         if ball.vel_vector[0] != 0:
-            times[0] = (0 + ball.r - ball.x) / ball.vel_vector[0]
-            times[1] = (DIM_BOARD_X - ball.r - ball.x) / ball.vel_vector[0]
+            times[0] = (0 + ball.r - ball.pos[0]) / ball.vel_vector[0]
+            times[1] = (DIM_BOARD_X - ball.r - ball.pos[0]) / ball.vel_vector[0]
         
         if ball.vel_vector[1] != 0:
-            times[2] = (0 + ball.r - ball.y) / ball.vel_vector[1]
-            times[3] = (DIM_BOARD_Y - ball.r - ball.y) / ball.vel_vector[1]
+            times[2] = (0 + ball.r - ball.pos[1]) / ball.vel_vector[1]
+            times[3] = (DIM_BOARD_Y - ball.r - ball.pos[1]) / ball.vel_vector[1]
 
         col_time = 1000000
         wall = None # wall 0 - left, wall 1 - right, wall 2 - bottom, wall 3 - top
@@ -139,55 +139,50 @@ class Table():
         
     def generate_cords(self):
         r = random.randint(20, 60)
-        x = random.randint(r, DIM_BOARD_X - r)
-        y = random.randint(r, DIM_BOARD_Y - r)
+        pos = np.array([random.uniform(r, DIM_BOARD_X - r), random.uniform(r, DIM_BOARD_Y - r)])
 
-        return (x, y, r)
+        return (pos, r)
 
-    def new_ball_cords_good(self, cords):
+    def new_ball_cords_good(self, pos, r):
         for ball in self.balls:
-            if (ball.x - cords[0]) ** 2 + (ball.y - cords[1]) ** 2 <= (ball.r + cords[2]) ** 2:
+            if np.linalg.norm(ball.pos - pos) <= (ball.r + r):
                 return False
         
         return True
     
     def generate_random_ball(self, ball_num):
         for ballID in range (ball_num):
-            cords = self.generate_cords()
+            pos, r = self.generate_cords()
 
-            while not self.new_ball_cords_good(cords):
-                cords = self.generate_cords()
+            while not self.new_ball_cords_good(pos, r):
+                pos, r = self.generate_cords()
 
             self.balls.append(Ball(
                 ballID,
-                cords[0],
-                cords[1],
-                cords[2],
+                pos,
+                r,
                 np.array([random.uniform(-1, 1), random.uniform(-1, 1)]),
                 (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
                 ))
 
     def check_ball_collisions(self):
         for ball1, ball2 in itertools.combinations(self.balls, 2):
-            if (ball2.x is not ball1.x and ball2.y is not ball1.y) and (ball1.x - ball2.x) ** 2 + (ball1.y - ball2.y) ** 2 <= (ball1.r + ball2.r) ** 2:
-                ball1.path.append((ball1.x, ball1.y))
-                ball2.path.append((ball2.x, ball2.y))
-                ball1.collisions.append((int((ball1.x + ball2.x)/2), int((ball1.y + ball2.y)/2)))
-                ball2.collisions.append((int((ball1.x + ball2.x)/2), int((ball1.y + ball2.y)/2)))
-                ball1.vel_vector, ball2.vel_vector = self.calculate_new_vectors(ball1.mass, ball2.mass, ball1.vel_vector, ball2.vel_vector, np.array([ball1.x, ball1.y]), np.array([ball2.x, ball2.y]))
+            if (ball1.ballID is not ball2.ballID) and np.linalg.norm(ball1.pos - ball2.pos) <= (ball1.r + ball2.r):
+                ball1.path.append(ball1.pos)
+                ball2.path.append(ball2.pos)
+                ball1.collisions.append((ball1.pos + ball2.pos)/2)
+                ball2.collisions.append((ball1.pos + ball2.pos)/2)
+                ball1.vel_vector, ball2.vel_vector = self.calculate_new_vectors(ball1.mass, ball2.mass, ball1.vel_vector, ball2.vel_vector, ball1.pos, ball2.pos)
             
 
-    def calculate_new_vectors(self, m1, m2, v1, v2, x1, x2):
-        v_new1 = v1 - 2 * m2/(m1 + m2) * np.dot(v1 - v2, x1 - x2)/((np.linalg.norm(x1 - x2)) ** 2)*(x1 - x2)
-        v_new2 = v2 - 2 * m1/(m1 + m2) * np.dot(v2 - v1, x2 - x1)/((np.linalg.norm(x2 - x1)) ** 2)*(x2 - x1)
+    def calculate_new_vectors(self, m1, m2, v1, v2, pos1, pos2):
+        v_new1 = v1 - 2 * m2/(m1 + m2) * np.dot(v1 - v2, pos1 - pos2)/((np.linalg.norm(pos1 - pos2)) ** 2)*(pos1 - pos2)
+        v_new2 = v2 - 2 * m1/(m1 + m2) * np.dot(v2 - v1, pos2 - pos1)/((np.linalg.norm(pos2 - pos1)) ** 2)*(pos2 - pos1)
         return (v_new1, v_new2)
 
     def apply_friction(self):
         for ball in self.balls:
             ball.vel_vector *= self.friction 
-            # if abs(ball.vel_vector[0]) < 0.0002 and abs(ball.vel_vector[1]) < 0.0002:
-            #     ball.vel_vector[0] = 0
-            #     ball.vel_vector[1] = 0
 
     def balls_stopped(self):
         for ball in self.balls:
@@ -198,34 +193,29 @@ class Table():
 
 
 class Ball():
-    def __init__(self, ballID, x, y, r, vel_vector, color):
+    def __init__(self, ballID, pos, r, vel_vector, color):
         self.ballID = ballID
-        self.x = x
-        self.y = y
+        self.pos = pos
         self.r = r
         self.mass = r
         self.color = color
         self.vel_multiplier = 2
         self.vel_vector = self.vel_multiplier * vel_vector #vel for tick
-        self.path = [(x, y)]
+        self.path = [pos]
         self.collisions = []
         self.predicted_collisions = []
     
     def update_ball(self, t_time):
-        # self.x += t_time * self.vel_vector[0]
-        # self.y += t_time * self.vel_vector[1]
-
-        self.x += self.vel_vector[0]
-        self.y += self.vel_vector[1]
+        self.pos += self.vel_vector
 
     def check_wall_collisions(self):
-        if self.x >= (DIM_BOARD_X - self.r) or self.x <= self.r:
+        if self.pos[0] >= (DIM_BOARD_X - self.r) or self.pos[0] <= self.r:
             self.vel_vector[0] = -1 * self.vel_vector[0]
-            self.path.append((self.x, self.y))
+            self.path.append(self.pos)
             
-        if self.y >= (DIM_BOARD_Y - self.r) or self.y <= self.r:
+        if self.pos[1] >= (DIM_BOARD_Y - self.r) or self.pos[1] <= self.r:
             self.vel_vector[1] = -1 * self.vel_vector[1]
-            self.path.append((self.x, self.y))
+            self.path.append(self.pos)
 
     
         
@@ -242,7 +232,7 @@ def render(screen, table):
 
     for ball in table.balls:
         # print(ball.x, ball.y)
-        pygame.draw.circle(screen, ball.color, [ball.x, DIM_BOARD_Y - ball.y], ball.r)
+        pygame.draw.circle(screen, ball.color, [ball.pos[0], DIM_BOARD_Y - ball.pos[1]], ball.r)
 
     # for ball in table.balls[0:1]:
     #     local_path = ball.path.copy()
@@ -266,8 +256,8 @@ table.generate_random_ball(5)
 
 # table.add_ball(100/4*multiplier, 200/4*multiplier + DIM_BALL_R, DIM_BALL_R, np.array([0.5,0.01]))
 
-# table.add_ball(1, 100, 200 + DIM_BALL_R, DIM_BALL_R, np.array([1.,0.1]), (255,0,0))
-# table.add_ball(2, 500, 200 + DIM_BALL_R, DIM_BALL_R, np.array([-1.,0.]), (0,120,0))
+# table.add_ball(1, np.array([100, 200]) + DIM_BALL_R, DIM_BALL_R, np.array([1.,0.1]), (255,0,0))
+# table.add_ball(2, np.array([500, 200]) + DIM_BALL_R, DIM_BALL_R, np.array([-1.,0.]), (0,120,0))
 
 
 # table.add_ball(500, 100 + DIM_BALL_R, DIM_BALL_R, np.array([-1.,1.]))
@@ -295,6 +285,7 @@ while not table.balls_stopped():
     # print("\n")
     # print(table.balls[0].vel_vector)
     table.t_time+=1
+    print(table.t_time)
     table.update_table()
     render(screen, table)
     pygame.display.flip()

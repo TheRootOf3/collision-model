09554273a0932b829e7a8f9366d68d7baa729d12
TheRootOf3 <andrzej.szablewski.20@ucl.ac.uk>
2021-03-09 22:21:32 +0100

    Added border lines in rendering method

--- View_tkinter.py
+++ View_tkinter.py
@@ -21,6 +21,11 @@ class View():
 
     def render_from_list(self, table, t_time):
         self.canvas.delete("all")
+        self.canvas.create_line(0, 0, self.dimX, 0, fill='#00FFFF')
+        self.canvas.create_line(0, 0, 0, self.dimY, fill='#00FF00')
+        self.canvas.create_line(0, self.dimY-1, self.dimX, self.dimY-1, fill='#FF00FF')
+        self.canvas.create_line(self.dimX-1, 0, self.dimX - 1, self.dimY, fill='#FFFF00')
+        
         ball_set = table.create_ball_set_to_render(t_time)
 
         for ball in ball_set:
